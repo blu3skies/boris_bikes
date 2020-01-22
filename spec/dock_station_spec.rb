34643b805b7cae88c4356a5031d3d95751b59237
Joe Ellingworth <joeellingworth@gmail.com>
2020-01-22 17:41:36 +0000

    step 11 test passing

--- spec/dock_station_spec.rb
+++ spec/dock_station_spec.rb
@@ -1,5 +1,23 @@
 require 'docking_station'
 
 describe DockingStation do
- it { should respond_to(:release_bike) }
+ it { is_expected.to respond_to(:release_bike) }
+
+ it 'should dock something' do 
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike 
+ end 
+
+ it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+ end 
+
+
+ it 'release working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+ end
 end
+
